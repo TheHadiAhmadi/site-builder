4dc91c59448db5efd759961992e93580189acd6b
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-08-20 18:56:55 +0430

    more cleanup

--- src/handlers.js
+++ src/handlers.js
@@ -1,4 +1,3 @@
-import { db } from "#services"
 import ai from "./handlers/ai.js"
 import collection from "./handlers/collection.js"
 import content from "./handlers/content.js"
@@ -9,7 +8,6 @@ import settings from "./handlers/settings.js"
 import setup from "./handlers/setup.js"
 import table from "./handlers/table.js"
 import user from "./handlers/user.js"
-import { DataTable } from "./pages/dataTable.js"
 
 export default {
     module,
