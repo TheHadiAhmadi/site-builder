f865d38dddc643915fcb7da1bc3f32f9b8f03519
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-08-28 01:08:46 +0430

    fix error

--- src/page.js
+++ src/page.js
@@ -151,7 +151,7 @@ async function sidebarPages(pages, currentPage, { permissions }) {
         <div data-sidebar-body>
             
             ${await Promise.all(pages.map(async x => `
-                <div data-sidebar-item ${currentPage.id === x.id ? 'data-active' : ''} data-sidebar-page-item data-action="navigation.link" data-href="${await getPageSlug(x)}?mode=edit">
+                <div data-sidebar-item ${currentPage?.id === x.id ? 'data-active' : ''} data-sidebar-page-item data-action="navigation.link" data-href="${await getPageSlug(x)}?mode=edit">
                     <div data-page-item-start>
                         <span  style="white-space: nowrap;">${x.name}</span>
                         <span data-page-item-slug>${x.slug}</span>
@@ -241,7 +241,7 @@ export async function renderBody(body, { props, mode, url, view, params, ...quer
     console.log(res, url.split('?')[0])
     let currentPage = res.page
 
-    if (!currentPage && view === 'iframe') view = 'pages.create'
+    if (!currentPage && (view === 'iframe' || !view)) view = 'pages.create'
 
     if (view === 'iframe' || !view) {
         sidebar = 'modules'
@@ -252,13 +252,13 @@ export async function renderBody(body, { props, mode, url, view, params, ...quer
                 <div data-iframe-header>
                     <div data-iframe-actions>
                         <div data-iframe-actions-start>
-                            <div data-iframe-title>${currentPage.name}</div>
+                            <div data-iframe-title>${currentPage?.name ?? 'Untitled'}</div>
                         </div>
                         <div data-iframe-actions-end>
-                            ${currentPage.dynamic ? await DynamicPageSelect(currentPage, props.params) : ''}
+                            ${currentPage?.dynamic ? await DynamicPageSelect(currentPage, props.params) : ''}
                             
                             ${view === 'iframe' ? `
-                                <a data-enhance data-button data-button-color="default" href="${getUrl({ view: 'pages.update', id: currentPage.id, back: encodeURIComponent('?mode=edit&view=iframe') })}">
+                                <a data-enhance data-button data-button-color="default" href="${getUrl({ view: 'pages.update', id: currentPage?.id, back: encodeURIComponent('?mode=edit&view=iframe') })}">
                                     <svg data-icon data-icon-size="sm" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m9.25 22l-.4-3.2q-.325-.125-.612-.3t-.563-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.338v-.675q0-.163.025-.338L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375t.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3t.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.338v.674q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375t-.6.3l-.4 3.2zM11 20h1.975l.35-2.65q.775-.2 1.438-.587t1.212-.938l2.475 1.025l.975-1.7l-2.15-1.625q.125-.35.175-.737T17.5 12t-.05-.787t-.175-.738l2.15-1.625l-.975-1.7l-2.475 1.05q-.55-.575-1.212-.962t-1.438-.588L13 4h-1.975l-.35 2.65q-.775.2-1.437.588t-1.213.937L5.55 7.15l-.975 1.7l2.15 1.6q-.125.375-.175.75t-.05.8q0 .4.05.775t.175.75l-2.15 1.625l.975 1.7l2.475-1.05q.55.575 1.213.963t1.437.587zm1.05-4.5q1.45 0 2.475-1.025T15.55 12t-1.025-2.475T12.05 8.5q-1.475 0-2.488 1.025T8.55 12t1.013 2.475T12.05 15.5M12 12"/></svg>    
                                     Page Settings
                                 </a>
@@ -520,7 +520,7 @@ export async function renderBody(body, { props, mode, url, view, params, ...quer
         params
     }
 
-    return `<div data-body data-page-id="${currentPage.id}">
+    return `<div data-body data-page-id="${currentPage?.id}">
         ${(await Promise.all(body.map(x => renderModule(x, { props, mode, definitions, permissions, request })))).join('')}
             ${previewContent}
         </div>`
